6a671dfec0e9e9d4fef9a62c1d243f1c438ce2cd
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-05 03:41:23 -0800

    Update mongo.py

--- MOONMUSIC/core/mongo.py
+++ MOONMUSIC/core/mongo.py
@@ -11,10 +11,10 @@ TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐕𝐈𝐏 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐌𝐎𝐎𝐍 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
     )
     temp_client = Client(
-        "VIPMUSIC",
+        "MOONMUSIC",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -30,5 +30,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.VIPMUSIC
-    pymongodb = _mongo_sync_.VIPMUSIC
+    mongodb = _mongo_async_.MOONMUSIC
+    pymongodb = _mongo_sync_.MOONMUSIC
