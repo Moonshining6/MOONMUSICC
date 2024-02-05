63834c76bed73a663f93b0d459075d9c0eac8875
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-05 03:30:47 -0800

    Update bot.py

--- MOONMUSIC/core/bot.py
+++ MOONMUSIC/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class VIP(Client):
+class MOON(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="VIPMUSIC",
+            name="MOONMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
