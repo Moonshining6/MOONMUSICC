f014fa5369b5b12aec62d2fe487297b8267a5056
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 05:04:17 -0800

    Update pause.py

--- MOONMUSIC/plugins/admins/pause.py
+++ MOONMUSIC/plugins/admins/pause.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.utils.database import is_music_playing, music_off
-from VIPMUSIC.utils.decorators import AdminRightsCheck
-from VIPMUSIC.utils.inline import close_markup
+from MOONMUSIC import app
+from MOONMUSIC.core.call import VIP
+from MOONMUSIC.utils.database import is_music_playing, music_off
+from MOONMUSIC.utils.decorators import AdminRightsCheck
+from MOONMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
