84effd31155fb2993b70b880ec6408ac4950cdbd
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 05:54:03 -0800

    Update watcher.py

--- MOONMUSIC/plugins/misc/watcher.py
+++ MOONMUSIC/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
+from MOONMUSIC import app
+from MOONMUSIC.core.call import MOON
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await VIP.stop_stream_force(message.chat.id)
+    await MOON.stop_stream_force(message.chat.id)
