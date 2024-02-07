a6844cb3c6d798f6667a9daf67153752341d77c8
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 05:01:49 -0800

    Update gpdata.py

--- MOONMUSIC/plugins/admins/gpdata.py
+++ MOONMUSIC/plugins/admins/gpdata.py
@@ -1,11 +1,11 @@
 from pyrogram import enums
 from pyrogram.enums import ChatType
 from pyrogram import filters, Client
-from VIPMUSIC import app
+from MOONMUSIC import app
 from config import OWNER_ID
-from VIPMUSIC.misc import SUDOERS
+from MOONMUSIC.misc import SUDOERS
 from pyrogram.types import Message
-from VIPMUSIC.utils.vip_ban import admin_filter
+from MOONMUSIC.utils.vip_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
