4df63650fd1374ee1cbbff203f392a41b04f188b
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 04:52:51 -0800

    Update Youtube.py

--- MOONMUSIC/platforms/Youtube.py
+++ MOONMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from VIPMUSIC.utils.database import is_on_off
-from VIPMUSIC.utils.formatters import time_to_seconds
+from MOONMUSIC.utils.database import is_on_off
+from MOONMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
