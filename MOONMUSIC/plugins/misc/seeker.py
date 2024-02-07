39e93b2995e461de80d0ffcfba6b70f354f9f563
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 05:52:53 -0800

    Update seeker.py

--- MOONMUSIC/plugins/misc/seeker.py
+++ MOONMUSIC/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from VIPMUSIC.misc import db
-from VIPMUSIC.utils.database import get_active_chats, is_music_playing
+from MOONMUSIC.misc import db
+from MOONMUSIC.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
