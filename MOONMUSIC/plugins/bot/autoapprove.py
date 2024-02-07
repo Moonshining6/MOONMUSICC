8d574f56ed815e8bb9da6c6d6adabc9c9f725dba
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 05:25:30 -0800

    Update autoapprove.py

--- MOONMUSIC/plugins/bot/autoapprove.py
+++ MOONMUSIC/plugins/bot/autoapprove.py
@@ -1,4 +1,4 @@
-from VIPMUSIC import app
+from MOONMUSIC import app
 from os import environ
 import random
 from pyrogram import Client, filters
@@ -13,11 +13,11 @@ from pyrogram.enums import ParseMode
 from typing import Union, Optional
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
 ]
 
 # --------------------------------------------------------------------------------- #
@@ -69,8 +69,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "VIPMUSIC/assets/userinfo.png"
-font_path = "VIPMUSIC/assets/hiroko.ttf"
+bg_path = "MOONMUSIC/assets/userinfo.png"
+font_path = "MOONMUSIC/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
@@ -83,9 +83,9 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # List of random photo links
 random_photo_links = [
-    "https://telegra.ph/file/ca950c0b8316b968957fa.jpg",
-    "https://telegra.ph/file/ca950c0b8316b968957fa.jpg",
-    "https://telegra.ph/file/ca950c0b8316b968957fa.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
+    "https://telegra.ph/file/aa015718299510040d41f.jpg",
     # Add more links as needed
 ]
 
