e2d2871e7acb3a0cccfdb883ea11a1d0f77aa48d
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 00:11:16 -0800

    Update quote.py

--- MOONMUSIC/plugins/tools/quote.py
+++ MOONMUSIC/plugins/tools/quote.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from VIPMUSIC import app
+from MOONMUSIC import app
 from httpx import AsyncClient, Timeout
 
 
