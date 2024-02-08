35057f2745c4cea023412ac76ce17f2ef0ce46f2
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 07:14:21 -0800

    Update stop.py

--- MOONMUSIC/plugins/admins/stop.py
+++ MOONMUSIC/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from MOONMUSIC import app
-from MOONMUSIC.core.call import VIP
+from MOONMUSIC.core.call import MOON
 from MOONMUSIC.utils.database import set_loop
 from MOONMUSIC.utils.decorators import AdminRightsCheck
 from MOONMUSIC.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await VIP.stop_stream(chat_id)
+    await MOON.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
