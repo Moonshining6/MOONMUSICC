bdb9d51034627c6faf852c7f6e4d7cde1b72f2d4
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 03:46:14 -0800

    Update extraction.py

--- MOONMUSIC/utils/extraction.py
+++ MOONMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from VIPMUSIC import app
+from MOONMUSIC import app
 
 
 async def extract_user(m: Message) -> User:
