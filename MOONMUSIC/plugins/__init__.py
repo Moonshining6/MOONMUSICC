038530d7c0bfe7de9eb8570952baf22ccdc03ee1
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 06:09:15 -0800

    Update __init__.py

--- MOONMUSIC/plugins/__init__.py
+++ MOONMUSIC/plugins/__init__.py
@@ -3,7 +3,7 @@ from os.path import dirname, isfile
 
 
 def list_all_modules():
-    work_dir = dirname(__file)
+    work_dir = dirname(__file__)
     mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
