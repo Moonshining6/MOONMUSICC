41b2aa804588d1cf92374d0dd22388dd6a318b4c
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 23:40:29 -0800

    Update Mongo_chk.py

--- MOONMUSIC/plugins/tools/Mongo_chk.py
+++ MOONMUSIC/plugins/tools/Mongo_chk.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from VIPMUSIC import app
+from MOONMUSIC import app
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
