7edfc0384c515f1fffbc7aac6757d3d3eb256235
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 07:08:38 -0800

    Update purge.py

--- MOONMUSIC/plugins/admins/purge.py
+++ MOONMUSIC/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from MOONMUSIC.utils.vip_ban import admin_filter
+from MOONMUSIC.utils.moon_ban import admin_filter
 from MOONMUSIC import app
 
 
