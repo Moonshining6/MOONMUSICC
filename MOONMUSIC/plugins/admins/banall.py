014c359f9e6f545d397935277eb8788594619f40
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 06:21:30 -0800

    Update banall.py

--- MOONMUSIC/plugins/admins/banall.py
+++ MOONMUSIC/plugins/admins/banall.py
@@ -2,7 +2,7 @@ from MOONMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from MOONMUSIC.utils.vip_ban import admin_filter
+from MOONMUSIC.utils.moon_ban import admin_filter
 from MOONMUSIC.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
