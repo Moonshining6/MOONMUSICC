7647160e1e06c6072f11faf6f5512a7a318fa063
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 03:49:53 -0800

    Update pastebin.py

--- MOONMUSIC/utils/pastebin.py
+++ MOONMUSIC/utils/pastebin.py
@@ -13,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def VIPBin(text):
+async def MOONBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
