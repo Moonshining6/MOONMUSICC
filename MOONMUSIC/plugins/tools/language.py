ba03b02ae7e187c876d4c84fbb0e95cb1f5eebf6
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-07 23:48:29 -0800

    Update language.py

--- MOONMUSIC/plugins/tools/language.py
+++ MOONMUSIC/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from VIPMUSIC import app
-from VIPMUSIC.utils.database import get_lang, set_lang
-from VIPMUSIC.utils.decorators import ActualAdminCB, language, languageCB
+from MOONMUSIC import app
+from MOONMUSIC.utils.database import get_lang, set_lang
+from MOONMUSIC.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
