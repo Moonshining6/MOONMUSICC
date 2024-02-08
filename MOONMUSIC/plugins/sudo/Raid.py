fc48a962cbb3d5929f19395c949f5010511ba7d6
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 08:17:08 -0800

    Update Raid.py

--- MOONMUSIC/plugins/sudo/Raid.py
+++ MOONMUSIC/plugins/sudo/Raid.py
@@ -32,7 +32,7 @@ def spam_command(client, message):
         for _ in range(num_times):
             # Send the spam message to the Telegram chat and mention the user
             message.reply_text(f"{user_to_tag} **{text_to_spam}**")
-            time.sleep(1)  # Add a delay between spam messages
+            time.sleep(0.5)  # Add a delay between spam messages
     elif message.reply_to_message:
         # If no text is provided with the spam command, spam the replied user's message
         user_to_tag = message.reply_to_message.from_user.mention()
