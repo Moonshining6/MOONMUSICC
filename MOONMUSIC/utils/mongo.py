f3209a5e8d5441e1e57925361df4782a125b96bb
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 03:49:34 -0800

    Update mongo.py

--- MOONMUSIC/utils/mongo.py
+++ MOONMUSIC/utils/mongo.py
@@ -7,7 +7,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.VIPMUSIC
+db = mongo.MOONMUSIC
 
 coupledb = db.couple
 
