3e37debb76b767333949b867745d1033d3581696
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 07:55:47 -0800

    Update reload.py

--- MOONMUSIC/plugins/tools/reload.py
+++ MOONMUSIC/plugins/tools/reload.py
@@ -65,7 +65,7 @@ async def restartbot(client, message: Message, _):
     await asyncio.sleep(1)
     try:
         db[message.chat.id] = []
-        await VIP.stop_stream_force(message.chat.id)
+        await MOON.stop_stream_force(message.chat.id)
     except:
         pass
     userbot = await get_assistant(message.chat.id)
@@ -92,7 +92,7 @@ async def restartbot(client, message: Message, _):
             pass
         try:
             db[chat_id] = []
-            await VIP.stop_stream_force(chat_id)
+            await MOON.stop_stream_force(chat_id)
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
