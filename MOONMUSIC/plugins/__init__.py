e394a74877d5fcb8a0e42581582f3f3b9ffeedff
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-08 06:12:28 -0800

    Update __init__.py

--- MOONMUSIC/plugins/__init__.py
+++ MOONMUSIC/plugins/__init__.py
@@ -2,18 +2,18 @@ import glob
 from os.path import dirname, isfile
 
 
-def list_all_modules():
+def __list_all_modules():
     work_dir = dirname(__file__)
     mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
         (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("init.py")
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
 
     return all_modules
 
 
-ALL_MODULES = sorted(list_all_modules())
-__all = ALL_MODULES + ["ALL_MODULES"]
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
